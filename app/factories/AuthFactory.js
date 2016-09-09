0914277f2b35467e60e3d294d6ea14e4a02310f0
Casey Reed <casey.w.reed@gmail.com>
2016-09-08 21:02:26 -0500

    userTokens stored and formatted in cached variable

--- app/factories/AuthFactory.js
+++ app/factories/AuthFactory.js
@@ -4,10 +4,7 @@ app.factory("AuthFactory", function ($q, $http, DiscogsCreds) {
 
     let _uid = null
     let user_agent = "CuePoint/0.01 +https://github.com/caseywreed/cue-point"
-    // let oauth_consumer_key = "RLNRPrabhetprjFlZgUt"
-    // let oauth_signature = "kuwUTbYZgyBKdsqfpdIRTfvxIFwAWbMw"
-    let oauth_token_secret = null
-    let oauth_token = null
+    let userTokens = {}
 
     let url = "https://api.discogs.com/oauth/request_token"
 
@@ -24,22 +21,45 @@ app.factory("AuthFactory", function ($q, $http, DiscogsCreds) {
 
     let discogsAuthCall = () => {
         console.log("discogsAuthCall running")
-        console.log("DiscogsCreds", DiscogsCreds)
         let timestamp = Date.now()
         return $q((resolve, reject) => {
             $http({
                 method: "GET",
-                url: "https://api.discogs.com/oauth/request_token?oauth_consumer_key=" + DiscogsCreds.key + "&oauth_signature_method=PLAINTEXT&oauth_timestamp=" + timestamp + "&oauth_nonce=33u0UT&oauth_version=1.0&oauth_signature=" + DiscogsCreds.oauth_signature + "%26"
+                url: "https://api.discogs.com/oauth/request_token?oauth_consumer_key="
+                + DiscogsCreds.key +
+                "&oauth_signature_method=PLAINTEXT&oauth_timestamp="
+                + timestamp +
+                "&oauth_nonce=33u0UT&oauth_version=1.0&oauth_signature="
+                + DiscogsCreds.oauth_signature +
+                "%26", //This is the ampersand at the end of the secret!
             })
             .success((data) => {
-                // data = JSON.parse(data)
                 console.log(data)
+                setInitialUserTokens(data)
                 resolve(data)
             })
             .error((error) => {
                 reject(error)
             })
         })
+    }
+
+    let discogsVerify = () => {
+
+    }
+
+// I KNOW this can be done better with .map()
+// but I'll figure that out later
+
+    let setInitialUserTokens = (tokens) => {
+        let splitTokens = tokens.split("&")
+        let tempTokenArray = []
+        splitTokens.forEach( function (token) {
+            let tokenValue = token.split("=")
+            tempTokenArray.push(tokenValue[1])
+        })
+        userTokens.oauth_token_secret = tempTokenArray[0]
+        userTokens.oauth_token = tempTokenArray[1]
     }
 
     return {setUid, getUid, discogsAuthCall}
