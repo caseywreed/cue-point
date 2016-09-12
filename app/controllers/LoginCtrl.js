cb9dff74a4ee28692edf5bbf4264b839b765f95f
Casey Reed <casey.w.reed@gmail.com>
2016-09-12 14:01:08 -0500

    Commit before verify key attempts

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -1,11 +1,36 @@
 "use strict";
 
-app.controller("LoginCtrl", function ($scope, AuthFactory) {
+app.controller("LoginCtrl", function ($scope, $location, AuthFactory) {
+
+    $scope.userVerifier = {
+        key: ""
+    }
 
     $scope.loginToDiscogs = function () {
         console.log("loginToDiscogs running")
         AuthFactory.discogsAuthCall()
     }
 
+    $scope.discogsVerify = function () {
+        console.log("discogsVerify running")
+        AuthFactory.discogsVerifyCall($scope.userVerifier.key)
+    }
+
+    // RUN THIS AS AN INIT EVERY TIME LOGIN PAGE LOADS!
+
+    $scope.checkForAuthToken = function () {
+        // GRAB AUTH TOKEN
+        // SEND THE AUTH TOKEN TO STEP 4 OF DISCOGS LOGIN
+        // WITH DISCOGS VERIFY
+        console.log("checkForAuthToken running")
+        console.log("$location", $location)
+        let url = $location.$$absUrl
+        let splitUrl = url.split("&")
+        let verifyToken = splitUrl[1].split("#")
+        let userVerifyCode = verifyToken[0].split("=")
+        let finalUserVerifyCode = userVerifyCode[1]
+        console.log("finalUserVerifyCode", finalUserVerifyCode)
+        AuthFactory.discogsVerifyCall(finalUserVerifyCode)
+    }
 
 })
--- app/factories/AuthFactory.js
+++ app/factories/AuthFactory.js
@@ -1,11 +1,10 @@
 "use strict";
 
-app.factory("AuthFactory", function ($q, $http, DiscogsCreds) {
+app.factory("AuthFactory", function ($q, $http, DiscogsCreds, $window, $location) {
 
     let _uid = null
     let user_agent = "CuePoint/0.01 +https://github.com/caseywreed/cue-point"
     let userTokens = {}
-
     let url = "https://api.discogs.com/oauth/request_token"
 
     let setUid = function (uid) {
@@ -31,11 +30,12 @@ app.factory("AuthFactory", function ($q, $http, DiscogsCreds) {
                 + timestamp +
                 "&oauth_nonce=33u0UT&oauth_version=1.0&oauth_signature="
                 + DiscogsCreds.oauth_signature +
-                "%26", //This is the ampersand at the end of the secret!
+                "%26&oauth_callback=http://localhost:8080/#/redirect", //This is the ampersand at the end of the secret!
             })
             .success((data) => {
                 console.log(data)
                 setInitialUserTokens(data)
+                $window.location.href = `https://discogs.com/oauth/authorize?oauth_token=${userTokens.oauth_token}`;
                 resolve(data)
             })
             .error((error) => {
@@ -44,8 +44,37 @@ app.factory("AuthFactory", function ($q, $http, DiscogsCreds) {
         })
     }
 
-    let discogsVerify = () => {
+    //Leaving off here. Getting a 401 while trying to authorize
+    //Has to be a better way to do this
+    //Lots of progress, though
 
+    let discogsVerifyCall = (userVerifyKey) => {
+        console.log("discogsVerify running")
+        console.log("userVerifyKey", userVerifyKey)
+        let timestamp = Date.now()
+        return $q((resolve, reject) => {
+            $http({
+                method: "GET",
+                url: "https://api.discogs.com/oauth/access_token?oauth_consumer_key="
+                + DiscogsCreds.key +
+                "&oauth_signature_method=PLAINTEXT&oauth_token="
+                + userTokens.oauth_token +
+                "&oauth_timestamp="
+                + timestamp +
+                "&oauth_nonce=33u0UT&oauth_version=1.0&oauth_verifier="
+                + userVerifyKey +
+                "&oauth_signature="
+                + DiscogsCreds.oauth_signature +
+                "%26&oauth_callback=http://google.com" //This is the ampersand at the end of the secret!
+            })
+            .success((data) => {
+                console.log(data)
+                resolve(data)
+            })
+            .error((error) => {
+                reject(error)
+            })
+        })
     }
 
 // I KNOW this can be done better with .map()
@@ -62,6 +91,10 @@ app.factory("AuthFactory", function ($q, $http, DiscogsCreds) {
         userTokens.oauth_token = tempTokenArray[1]
     }
 
-    return {setUid, getUid, discogsAuthCall}
+    let getUserVerifyCode = (url) => {
+        let splitUrl = url.split("$")
+    }
+
+    return {setUid, getUid, discogsAuthCall, discogsVerifyCall, getUserVerifyCode}
 
 })
